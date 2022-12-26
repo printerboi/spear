1ab534a35786f3d3607edc57a2e7513c2702886e
Maximilian Krebs <maximilian-krebs@online.de>
2022-12-26 22:36:59 +0100

    Added commentary

--- src/include/Bencher/Bencher.h
+++ src/include/Bencher/Bencher.h
@@ -7,13 +7,32 @@
 
 #include "string"
 
+/**
+ * Simple class to benchmark the llvm-code and output the data in an appropriate format
+ * @property iterations Times the energy values will be executed. Needed for the average
+ * @property repetitions Times a single program will be executed
+ */
 class Bencher {
     public:
         int iterations;
         int repetitions;
+        /**
+         * Creates a Bencher object and sets the iterations and repetitions property according to the parameters
+         * @constructor
+         * @param it Iterations for the average
+         * @param rep Times a program will be executed repeatedly
+         */
         explicit Bencher(int it, int rep);
+        /**
+         * Runs the benchmark and returns the values for the benchmarked files
+         */
         void benchmark();
     private:
+        /**
+         * Benchmarks a single file, calculates the used energy and returns the calculated value
+         * @param file String to the file that needs to be benchmarked
+         * @return The used energy
+         */
         uint64_t benchmarkFile(std::string file);
 };
 
