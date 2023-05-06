36383df7d277e29a3ce707751d1638cf939fdd19
Maximilian Krebs <maximilian-krebs@online.de>
2023-05-06 16:42:00 +0200

    Updated outofbounds analysis and removed std::fill operation

--- programs/outofbounds/largeArrayInspector.cpp
+++ programs/outofbounds/largeArrayInspector.cpp
@@ -7,7 +7,7 @@ int main()
     int length = 2000 * 1024 * 1024;
     std::vector<char> elements(length);
 
-    std::fill(elements.begin(), elements.end(), 'c');
+    //std::fill(elements.begin(), elements.end(), 'c');
 
     std::cout << "First Element at address " << elements[0] << "\n";
     std::cout << "Last Element at address " << elements[length-1] << "\n";
