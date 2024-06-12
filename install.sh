48ef5b0665e9153045cf81ea43e69fc5c5ae569b
Maximilian Krebs <dev@maximiliankrebs.com>
2024-06-12 22:09:50 +0200

    fix: Fixed the installer script

--- install.sh
+++ install.sh
@@ -28,6 +28,38 @@ function compileFile() {
 }
 
 
+# Check the user id of the executing user -> ensures privileged rights
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+# Install dependencies
+if ! command -v clang-17 &> /dev/null
+then
+  # Install llvm-17
+  mkdir /opt/spear
+  wget https://apt.llvm.org/llvm-snapshot.gpg.key -O /opt/spear/apt.llvm.org.asc
+  gpg --output /opt/spear/apt.llvm.org.gpg --dearmor /opt/spear/apt.llvm.org.asc
+  mv apt.llvm.org.gpg /etc/apt/keyrings/
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/apt.llvm.org.gpg
+  sudo echo "Types: deb\n URIs: https://apt.llvm.org/bookworm/\n Suites: llvm-toolchain-bookworm llvm-toolchain-bookworm-17 llvm-toolchain-bookworm-16\n Components: main\n Signed-By: /etc/apt/keyrings/apt.llvm.org.gpg\n" /etc/apt/sources.list.d/apt.llvm.org.sources
+  sudo apt update
+  sudo apt install clang-17 lldb-17 lld-17 clangd-17 clang-tools-17
+  ln -s /usr/bin/clang-17 /usr/bin/clang
+  ln -s /usr/bin/clang++-17 /usr/bin/clang++
+  ln -s /usr/bin/llvm-as-17 /usr/bin/llvm-as
+  ln -s /usr/bin/llc-17 /usr/bin/llc
+fi
+
+if ! dpkg -s nlohmann-json3-dev &> /dev/null
+then
+  # Install nlohnmann-json
+  apt install nlohmann-json3-dev
+fi
+
+# Install SPEAR
 mkdir build
 cd build || exit
 echo "[1/6] Compiling SPEAR"
